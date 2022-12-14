b2de841b3ae24ef90fa8f0bb247676698fb8ab03
MICHELLE <omekemichelle@gmail.com>
2022-12-14 03:02:02 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+// Auto Slide   
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+   var slides = document.getElementsByClassName("mySlides");
+   for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+   }
+   slideIndex++;
+   if (slideIndex > slides.length) { slideIndex = 1 }
+   slides[slideIndex - 1].style.display = "block";
+   setTimeout(showSlides, 5000); // Change image every 4 seconds
+}
+
+const btns = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function (e) {
+    const id = e.target.dataset.id;
+    if (id) {
+        // remove active from other buttons
+        btns.forEach(function (btn) {
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+        // hide other articles
+        articles.forEach(function(article){
+            article.classList.remove("active")
+        })
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
+}); 
